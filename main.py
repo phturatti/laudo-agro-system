4a408d652bbe2d4d0c0fbbee59cb79e1e4dff5f3
phturatti <paulo.turatti@alternativahemp.com.br>
2025-07-15 00:44:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        paciente = request.form['paciente']
+        produto = request.form['produto']
+        dose = request.form['dose']
+        return f"Laudo para {paciente}: {produto}, {dose} mg/dia"
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
