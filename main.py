499d0e930df7c1e9269b0d00ab90bced6464ecb0
phturatti <paulo.turatti@alternativahemp.com.br>
2025-07-15 01:01:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,18 @@ def home():
         paciente = request.form['paciente']
         produto = request.form['produto']
         dose = request.form['dose']
-        return f"Laudo para {paciente}: {produto}, {dose} mg/dia"
-    return render_template('index.html')
+        return f'Laudo para {paciente}, {produto}, {dose} mg/dia'
+    return render_template('index.html')  # Certifique-se de que o arquivo index.html está no diretório 'templates'
+
+@app.route('/gerar', methods=['POST'])
+def gerar_laudo():
+    # Aqui você pode pegar os dados do formulário
+    paciente = request.form['paciente']
+    produto = request.form['produto']
+    dose = request.form['dose']
+    # Aqui você pode fazer o processamento para gerar o laudo ou retornar os dados
+    return f'Laudo gerado para {paciente}, produto: {produto}, dose: {dose} mg.'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
